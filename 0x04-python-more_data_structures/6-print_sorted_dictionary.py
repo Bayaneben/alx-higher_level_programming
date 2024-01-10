351ba367bf77839f105551d417efd2b162d120e3
Bayaneben <bayanebenhassan95@gmail.com>
2024-01-10 13:13:45 -0800

    second commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key in a_dictionary.keys():
-        if type(key) != str :
-            print('the key is not a string')
-            exit()
     for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
